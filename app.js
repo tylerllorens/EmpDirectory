5bf17f25dd692e77e721bf07b693f261976f59ab
Tyler Llorens <tylerllore@gmail.com>
2025-06-08 14:52:02 -0400

    Finished Code

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-const express = require("express");
+import express from "express";
 const app = express();
 export default app;
-// const PORT = process.env.PORT || 3000;
-import employees from "./db/employees.js";
+
+import employeesRouter from "#api/employees";
+//best to put .json at the top
+app.use(express.json());
 
 app.route("/").get((req, res) => {
   res.send("Hello employees!");
 });
 
-app.route("/employees").get((req, res) => {
-  res.send(employees);
-});
+app.use("/employees", employeesRouter);
 
-app.route("/employees/random").get((req, res) => {
-  const randomEmployee = Math.floor(Math.random() * employees.length);
-  res.json(employees[randomEmployee]);
-});
-
-app.route("/employees/:id").get((req, res) => {
-  const { id } = req.params;
-
-  const employee = employees.find((employee) => employee.id === +id);
-  if (!employee) {
-    return res.status(404).json("Employee not found");
-  }
-
-  res.json(employee);
+app.use((err, req, res, next) => {
+  res.status(500).send("Sorry, something went wrong :(");
 });
